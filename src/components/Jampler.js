11b9a7ee6f4a3b2e9e33d86b9497c8b0fb4badbd
famulus <famulus.fusion@gmail.com>
2018-02-04 14:25:22 -0500

    clean up main Jampler file

--- src/components/Jampler.js
+++ src/components/Jampler.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Patch from './Patch.js';
+import {Howl, Howler} from 'howler'
+// import sound from '../../sounds/test.mp3';
 
 
 export default class Jampler extends React.Component {
@@ -9,20 +11,24 @@ export default class Jampler extends React.Component {
 		
 
 
-
-
-
+		const sound = new Howl({
+		  src: ['https://archive.org/download/JoshJones-BreakBottles-OriginalDemomp3/Josh-BreakBottles.mp3'],
+		    sprite: {
+			    blast: [4200, 5000]
+			  }
+		});
 
 
 		const onMIDISuccess = (midiAccess) => {
 			console.log("okok")
       // when we get a succesful response, run this code
-      // # this is our raw MIDI data, inputs, outputs, and sysex status
+      //  this is our raw MIDI data, inputs, outputs, and sysex status
       const inputs = midiAccess.inputs.values()
-      // # loop over all available inputs and listen for any MIDI input
+      //  loop over all available inputs and listen for any MIDI input
       let input = inputs.next()
-      while (input &&  !input.done){
-	        // # each time there is a midi message call the onMIDIMessage function
+      // let input2 = inputs.next()
+      while ((input &&  !input.done)  ){
+        //  each time there is a midi message call the onMIDIMessage function
 	      input.value.onmidimessage = onMIDIMessage
 	      input = inputs.next()
       }
@@ -30,30 +36,26 @@ export default class Jampler extends React.Component {
     }
 
     const onMIDIFailure = (error) =>{
-
-			console.log("nono")
-      // # when we get a failed responsysex, run this code
+      //  when we get a failed responsysex, run this code
       console.log( "No access to MIDI devices or your browsysexr doesn\'t support WebMIDI API. Please use WebMIDIAPIShim " + error)
       return
     }
 
 
     const onMIDIMessage = (message) => {
-
     	const data = message.data
-      // # this gives us our [command/channel, note, velocity] data.
-      if  ((data[0] == 144) && (data[1] < 52)){
-
-      	console.log ('MIDI data', data)
+      //  this gives us our [command/channel, note, velocity] data.
+    	// console.log ('MIDI data', data)
+      if((data[0] == 144) && (data[1] < 52)){
       	let previous_state = this.state
-        // this.setState({currentPatch: (data[1] - 36)})
-        // this.set_current_patch()
-	      // # MIDI data [144, 63, 73]
+      	sound.play('blast')
 	      return
-      } //# the < 52 ignore the foot pedal
-
-
+      } 
+      if((data[0] == 128) && (data[2] == 0)){
+      	sound.stop()
+      }
     }
+
 
     if (navigator.requestMIDIAccess){
     	navigator.requestMIDIAccess({sysex: false}).then(onMIDISuccess, onMIDIFailure)
@@ -62,20 +64,11 @@ export default class Jampler extends React.Component {
     	alert ('No MIDI support in your browser.')
     }
 
-
-
-
-
-
-
-
-
-
 	}
 
-	    render() {
-    	return <div>I'm Jampler <Patch/><Patch/></div>
-    }
+	render() {
+		return <div>I'm Jampler <Patch/><Patch/></div>
+	}
 
 }
 
